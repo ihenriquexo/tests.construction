31724add8e8363fef48a6f1dbf06386e291fe6d7
ihenriquex <ihenriquexo@gmail.com>
2023-08-01 16:22:38 -0300

    final real arrays

--- arrays 6.js
+++ arrays 6.js
@@ -1,23 +1,33 @@
-//arrays 6
 nomes=[]
 notas=[]
 continuar=true
 let i = 0
+somaNotas=0
 while (continuar) {
     
         nome=prompt("digite o nome do "+(i)+"* aluno!")
         nomes[i]=nome
-        nota=prompt("digite a nota do "+(i)+"* aluno!")
+        nota=parseFloat(prompt("digite a nota do "+(i)+"* aluno!"))
         notas[i]=nota
-        i++
         
+        
+        i++
 
     gg=prompt("1=parar")
     if (gg=="1") {
         continuar=false
-    }}
-    console.log("nome "+nomes)
-    console.log("nota "+notas)
-
+    }
+}
+for (let i = 0; i < notas.length; i++) {
+    somaNotas=notas[i]+somaNotas;
     
+}
+
+
+mediaGer=somaNotas/nomes.length
+
+    console.log("nomes "+nomes)
+    console.log("notas "+notas)
+    console.log("soma das notas "+somaNotas)
+    console.log("media geral das notas "+mediaGer)
 
