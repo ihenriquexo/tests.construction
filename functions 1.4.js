02462ad73fa4c519d88d788ba88a59e2b8604f97
ihenriquex <ihenriquexo@gmail.com>
2023-08-02 13:32:12 -0300

    function 1.6

--- functions 1.4.js
+++ functions 1.4.js
@@ -12,17 +12,46 @@ function calcularMedia(num1, num2, num3){
     console.log("A media será calculada aqui")
     console.log((num1+num2+num3)/3)
 }
-//functions 1.4
-somarValores(a=parseInt(prompt("digite o numero a")),b=parseInt(prompt("digite o numero b")))
-subtrairValores(a,b)
-multiplicarValores(a,b)
-function somarValores(a,b) {
-    console.log("valores somados =",a+b)
+//functions 1.6
+c=[]
+d=true
+continuar=true
+while (continuar) {
+    if (d==true) {
+        a=parseInt(prompt("digite o numero a")),b=parseInt(prompt("digite o numero b"))
+    }
+    fafa=prompt("o que deseja fazer com esses valores? 1=+|2=-|3=*|4=/")
+if (fafa=="1") {
+    somarValores(a,b) 
+}else if (fafa="2") {
+    subtrairValores(a,b)
+}else if (fafa="3") {
+    multiplicarValores(a,b)
+}else if (fafa="4") {
+    dividirValores(a,b)
 }
-function subtrairValores(a,b) {
-    console.log("valores subtraidos =",a-b)
+cont=prompt("deseja fazer outra operação?1=n")
+if (cont=="1") {
+    continuar=false
+}else{
+perg=prompt("com os mesmos valores?1=s")
+if (perg=="1") {
+    d=false
 }
-function multiplicarValores(a,b) {
-    console.log("valores multiplicados =",a*b)
 }
-//
+}
+
+
+
+function somarValores() {
+    console.log("valores somados =",c=a+b)
+}
+function subtrairValores() {
+    console.log("valores subtraidos =",c=a-b)
+}
+function multiplicarValores() {
+    console.log("valores multiplicados =",c=a*b)
+}
+function dividirValores() {
+    console.log("valores divididos =",c=a/b)
+}
