548f545579f0e10250c8a11b747bb9b1f302268d
ihenriquex <ihenriquexo@gmail.com>
2023-08-02 12:48:51 -0300

    functions 1.4

--- functions 1.4.js
+++ functions 1.4.js
@@ -1,4 +1,18 @@
+//exemplo
+//metodo-função
 
+calcularMedia(2,10,20)//chamando|executando a função
+
+
+function calcularMedia(num1, num2, num3){
+    //num1=2
+    //num2=10
+    //num3=20
+    //criando a função
+    console.log("A media será calculada aqui")
+    console.log((num1+num2+num3)/3)
+}
+//functions 1.4
 somarValores(a=parseInt(prompt("digite o numero a")),b=parseInt(prompt("digite o numero b")))
 subtrairValores(a,b)
 multiplicarValores(a,b)
@@ -11,3 +25,4 @@ function subtrairValores(a,b) {
 function multiplicarValores(a,b) {
     console.log("valores multiplicados =",a*b)
 }
+//
