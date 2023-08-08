ea1080e412c444da28a180d9f5689d24d882526c
ihenriquex <ihenriquexo@gmail.com>
2023-08-07 23:36:12 -0300

    functions 4.4

--- function 4.1.js
+++ function 4.1.js
@@ -169,3 +169,16 @@ function porcentagemAte1500() {
     }
     return console.log(((contSalarios/salario.length)*100+"%"))
 }
+//function 4.4
+nota1=parseInt(prompt("digite sua "+1+"* nota"))
+nota2=parseInt(prompt("digite sua "+2+"* nota"))
+nota3=parseInt(prompt("digite sua "+3+"* nota"))
+op=prompt("a=media aritmetica|p=media ponderada")
+recebe()
+function recebe() {
+if (op=="a") {
+    return console.log((nota1+nota2+nota3)/3)
+}else if (op=="p"){
+    }return console.log((nota1*5+nota2*3+nota3*2)/10)
+
+}
--- teste.js
+++ teste.js
@@ -1,48 +1,13 @@
-salario=[1500,1600,3200,4000]
-numeroF=[1,0,3,2]
-porcentagemAte1500()
-MediaSalarial()
-MaiorSalario()
-//criar functions\mediaSalarial|v,mediadeFilhos|v,maiorSalario|
 
-function MediaSalarial() {
-    somaSalarios=0
-     for (let i = 0; i < numeroF.length; i++) {
-        somaSalarios+=salario[i]
-        
-     }medias=somaSalarios/(numeroF.length+1)
-        return console.log(medias)
-        
-    
-}
+    nota1=parseInt(prompt("digite sua "+1+"* nota"))
+    nota2=parseInt(prompt("digite sua "+2+"* nota"))
+    nota3=parseInt(prompt("digite sua "+3+"* nota"))
+    op=prompt("a=media aritmetica|p=media ponderada")
+    recebe()
+function recebe() {
+    if (op=="a") {
+        return console.log((nota1+nota2+nota3)/3)
+    }else if (op=="p"){
+        }return console.log((nota1*5+nota2*3+nota3*2)/10)
 
-function mediadeFilhos() {
-    somaFilhos=0
-     for (let i = 0; i < numeroF.length; i++) {
-        somaFilhos+=numeroF[i]
-        
-     }mediaf=somaFilhos/(numeroF.length+1)
-        return console.log(mediaf)
-        
-    
-}
-
-function MaiorSalario() {
-    maiorSalario=salario[0]
-     for (let i = 1; i < salario.length; i++) {
-        if (salario[i]>maiorSalario) {
-            maiorSalario=salario[i]
-        }
-    }
-    return console.log(maiorSalario)
-}
-function porcentagemAte1500() {
-    contSalarios=0
-    for (let o = 0; o < salario.length; o++) {
-        if (salario[o]<=1500) {
-            contSalarios++
-        }
-        
-    }
-    return console.log(((contSalarios/salario.length)*100+"%"))
 }
