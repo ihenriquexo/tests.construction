f531bd01f1e04531dc88ab22ddb6b0d2017c7039
ihenriquex <ihenriquexo@gmail.com>
2023-08-10 18:27:59 -0300

    functions 5.1 até 5.3

--- teste.js
+++ teste.js
@@ -1,19 +1,42 @@
-nota1=parseFloat(prompt("nota1"))
-nota2=parseFloat(prompt("nota2"))
-nota3=parseFloat(prompt("nota3"))
-letra=prompt("a=media aritmetica|p=media ponderada")
-recebedorDeNotas(nota1,nota2,nota3)
-function recebedorDeNotas() {
+ids=[]
+nomes=[]
+precos=[]
+avaliacoes=[]
+contador=0
+cadastrarProduto()
+id=prompt("qual o id?")
+buscarPorId(id)
+nome=prompt("qual o nome?")
+buscarPorNome(nome)
 
-    if (letra=="a") {
-        return console.log(((nota1+nota2+nota3)/3))
-    }else if(letra=="p"){
-        return console.log(((nota1*5)+(nota2*3)+(nota3*2))/10)
-    }
-            
-          
-    }
-            
-       
-        
+//exibe todos os produtos pelo id
+for (let o = 0; o < ids.length; o++) {
+    console.log(ids[o],nomes[o],precos[o],avaliacoes[o]);
     
+}
+
+
+
+function cadastrarProduto() {
+    var id=contador+1
+    nome=prompt("nome do produto")
+    preco=parseInt(prompt("preço do produto"))
+    avaliacao=prompt("avaliação do produto")
+    ids[contador]=id
+    nomes[contador]=nome
+    precos[contador]=preco
+    avaliacoes[contador]=avaliacao
+    contador++
+}
+function buscarPorId(id){
+    for (let i = 0; i < ids.length; i++) {
+        if (id==ids[i])
+        return console.log(ids[i],nomes[i],precos[i],avaliacoes[i])
+    }
+}
+function buscarPorNome(nome) {
+    for (let i = 0; i < nomes.length; i++) {
+        if (nome==nomes[i])
+        return console.log(ids[i])
+    }
+}
