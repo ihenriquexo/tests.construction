b21f5de7ed3360d1b40acdcec7e1f435708fd1d0
ihenriquex <ihenriquexo@gmail.com>
2023-08-10 17:54:14 -0300

    functions 4 completo

--- functions 4.1 e 4.2.js
+++ functions 4.1 e 4.2.js
@@ -37,3 +37,66 @@ function verificarNum() {
         };
     }return somaDivisores===numero
 }
+//functions 4.3
+salarios=[1200,4000,3200,1400]
+quantFilhos=[2,0,3,1]
+mediaSalario=0
+Pmenos1500=0
+mediaSalarial()
+mediaFilhos()
+oMaiorSalario()
+calcularPorcentagem()
+function mediaSalarial() {
+    let somaSalario=0
+    for (let i = 0; i < salarios.length; i++) {
+        somaSalario+=salarios[i]
+        
+        
+    }
+    return console.log(mediaSalario=somaSalario/salarios.length)
+}
+function mediaFilhos() {
+    let somaFilhos=0
+    for (let i = 0; i < quantFilhos.length; i++) {
+        somaFilhos+=quantFilhos[i]
+        
+        
+    }
+    return console.log(mediaFilho=somaFilhos/quantFilhos.length)
+}
+//obs
+function oMaiorSalario() {
+    maiorSalario=salarios[0]
+    for (let i = 1; i < salarios.length; i++) {
+        if (salarios[i]>maiorSalario) {
+            maiorSalario=salarios[i]
+            
+        }
+        
+    }
+    return console.log(maiorSalario)
+}
+function calcularPorcentagem() {
+    contSalar=0
+    for (let i = 0; i < salarios.length; i++) {
+        if (salarios[i]<=1500) {
+            contSalar++
+        }
+        
+    }
+    return console.log((contSalar/salarios.length)*100+"%")
+}
+//functions 4.4
+nota1=parseFloat(prompt("nota1"))
+nota2=parseFloat(prompt("nota2"))
+nota3=parseFloat(prompt("nota3"))
+letra=prompt("a=media aritmetica|p=media ponderada")
+recebedorDeNotas(nota1,nota2,nota3)
+function recebedorDeNotas() {
+
+    if (letra=="a") {
+        return console.log(((nota1+nota2+nota3)/3))
+    }else if(letra=="p"){
+        return console.log(((nota1*5)+(nota2*3)+(nota3*2))/10)
+    }   
+    }
